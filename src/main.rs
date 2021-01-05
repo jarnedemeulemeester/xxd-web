34f2a6deeab2dbdfee6af53bcbbb31a868d5182e
jarnedemeulemeester <jarne.demeulemeester@student.howest.be>
2021-01-05 19:49:25 +0100

    Create unique subdirectory for each session

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate actix_web;
+extern crate uuid;
 
-use std::env;
+//use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
@@ -10,11 +11,13 @@ use actix_files as fs;
 use actix_multipart::Multipart;
 use actix_web::{
     http::header::{self, DispositionParam, DispositionType},
-    middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Result,
+    middleware, web, App, HttpRequest, HttpServer, Result,
 };
 use fs::NamedFile;
 use futures::{StreamExt, TryStreamExt};
 use header::ContentDisposition;
+
+use uuid::Uuid;
 
 async fn index(_req: HttpRequest) -> Result<NamedFile> {
     let path: PathBuf = "./static/index.html".parse().unwrap();
@@ -23,15 +26,19 @@ async fn index(_req: HttpRequest) -> Result<NamedFile> {
 
 async fn xxd(mut payload: Multipart) -> Result<NamedFile> {
     // Maximum filesize in bytes
-    let mut max_filesize = 20971520;
-    if !env::var("MAX_FILESIZE").is_err() {
-        max_filesize = env::var("MAX_FILESIZE").unwrap().parse::<u64>().unwrap();
-    }
+    // let mut max_filesize = 20971520;
+    // if !env::var("MAX_FILESIZE").is_err() {
+    //     max_filesize = env::var("MAX_FILESIZE").unwrap().parse::<u64>().unwrap();
+    // }
+
+    let session_id = Uuid::new_v4();
+    std::fs::create_dir(format!("./tmp/{}", session_id))?;
 
     let mut field = payload.try_next().await.unwrap().unwrap();
     let content_type = field.content_disposition().unwrap();
     let filename = content_type.get_filename().unwrap();
-    let filepath = format!("./tmp/{}", sanitize_filename::sanitize(filename));
+    let filepath = format!("./tmp/{}/{}", session_id, sanitize_filename::sanitize(filename));
+
 
     let mut f = web::block(|| File::create(filepath)).await.unwrap();
 
@@ -43,10 +50,12 @@ async fn xxd(mut payload: Multipart) -> Result<NamedFile> {
     // Dump to hex and put in c array with xxd
     let status = Command::new("xxd")
         .arg("-i")
-        .arg(["./tmp/", sanitize_filename::sanitize(filename).as_str()].concat())
+        .arg(["./tmp/", session_id.to_string().as_str(), "/", sanitize_filename::sanitize(filename).as_str()].concat())
         .arg(
             [
                 "./tmp/",
+                session_id.to_string().as_str(),
+                "/",
                 sanitize_filename::sanitize(filename).as_str(),
                 ".cc",
             ]
@@ -56,10 +65,10 @@ async fn xxd(mut payload: Multipart) -> Result<NamedFile> {
         .expect("Failed");
     println!("Conversion exited with: {}", status);
 
-    std::fs::remove_file(["./tmp/", filename].concat());
+    std::fs::remove_file(["./tmp/", session_id.to_string().as_str(), "/", filename].concat()).unwrap();
 
     let final_filename = [filename, ".cc"].concat();
-    let path: PathBuf = ["./tmp/", final_filename.as_str()]
+    let path: PathBuf = ["./tmp/", session_id.to_string().as_str(), "/", final_filename.as_str()]
         .concat()
         .parse()
         .unwrap();
